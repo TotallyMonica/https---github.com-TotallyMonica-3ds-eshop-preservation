d1840a3579fe7011b22f3996c5572ae408c1d14f
BuildTools <wolfmurdurer23@gmail.com>
2022-02-21 17:39:32 -0600

    Now able to list just the titles

--- List3dsGames.py
+++ List3dsGames.py
@@ -1,17 +1,16 @@
 import csv
+import json
 
 # import 3DS.csv as games
-def importGames (gameCSV):
+def importGames (gameJSON):
     print("Games CSV was successfully imported.")
 
-def listGames (gameCSV):
+def listGames (gameJSON):
     print("The imported games are as listed here: ")
-    for entry in gameCSV:
-        print(entry["Title"])
+    for entry in gameJSON:
+        print(entry['Title'])
 
 if __name__ == "__main__":
-    games = open('3DS.csv','r')
+    games = json.load(open('3DS.json','r'))
     importGames(games)
     listGames(games)
-    games.close()
-    

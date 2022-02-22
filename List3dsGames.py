f28dbd43f6d8cf66077d0cfb702e2c39342b94e7
BuildTools <wolfmurdurer23@gmail.com>
2022-02-21 18:05:58 -0600

    Added region game count & listing games in region

--- List3dsGames.py
+++ List3dsGames.py
@@ -1,4 +1,3 @@
-import csv
 import json
 
 # import 3DS.csv as games
@@ -10,7 +9,70 @@ def listGames (gameJSON):
     for entry in gameJSON:
         print(entry['Title'])
 
+def regionGameCount (gameJSON):
+    NA = 0
+    JPN = 0
+    PAL = 0
+    Asia = 0
+    errors = 0
+
+    for entry in gameJSON:
+        try:
+            if not entry['Release date Japan'] == 'Unreleased':
+                JPN = JPN + 1
+            if not entry['Release date NA'] == 'Unreleased':
+                NA = NA + 1
+            if not entry['Release date Europe'] == 'Unreleased':
+                PAL = PAL + 1
+            if not entry['Release date Australasia'] == 'Unreleased':
+                Asia = Asia + 1
+        except:
+            errors = errors + 1
+    
+    print(f"Japan has {JPN} games, North America has {NA} games, Europe has {PAL} games, and Australia/Asia has {Asia} games")
+    print(f"{errors} error(s) were caught and dismissed while running")
+
+def gamesInRegion (gameJSON):
+    JPN = []
+    NA = []
+    PAL = []
+    Asia = []
+    errors = []
+
+    i = 0
+
+
+    for entry in gameJSON:
+        try:
+            if not entry['Release date Japan'] == 'Unreleased':
+                JPN.append(i)
+            if not entry['Release date NA'] == 'Unreleased':
+                NA.append(i)
+            if not entry['Release date Europe'] == 'Unreleased':
+                PAL.append(i)
+            if not entry['Release date Australasia'] == 'Unreleased':
+                Asia.append(i)
+        except:
+            errors.append(i)
+
+        i = i + 1
+
+    print("Japan has the following games:")
+    for entry in JPN:
+        print(gameJSON[entry]['Title'])
+    print("North America has the following games:")
+    for entry in NA:
+        print(gameJSON[entry]['Title'])
+    print("Europe has the following games:")
+    for entry in PAL:
+        print(gameJSON[entry]['Title'])
+    print("Asia/Australia has the following games:")
+    for entry in Asia:
+        print(gameJSON[entry]['Title'])
+
 if __name__ == "__main__":
     games = json.load(open('3DS.json','r'))
     importGames(games)
     listGames(games)
+    regionGameCount(games)
+    gamesInRegion(games)
